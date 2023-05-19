13307940643482f592123bc669fcfe98343fb1e1
wortex1337 <kanov.ilya456@gmail.com>
2023-05-19 22:28:44 +0300

    Изменил текст кнопки

--- test/src/components/test-count.jsx
+++ test/src/components/test-count.jsx
@@ -12,7 +12,7 @@ const [count, setCount] = useState(0)
     return (
         <div>
             <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}>Клик!</button>
+            <button onClick={() => setCount(count + 1)}>НАЖМИ НА МЕНЯ</button>
         </div>
     )
 }
